6ed69a880be4693f8966b6c65e02ada2e4706fea
shuteye <daniel.evans.12@ucl.ac.uk>
2014-04-25 12:03:04 +0100

    1st draft change

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,24 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Programming Assignment #2 : 
+## The following functions perform matrix inverstion with the added benefit of 
+## caching previously performed inversions in order to save CPU time
 
-## Write a short comment describing this function
+## The first function takes a matrix and creates a series of functions
+## for the purpose of caching and retrieving invertered matrices
+##
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    mcm<-NULL
+    set<- function(y) {
+      x<<-y
+      mcm<<-NULL
+    }
+    get<-function() x
+    setinv<-function(inv) mcm<<-inv
+    getinv<-function() mcm
+    
+    val<-list(set=set, get=get, setinv=setinv, getinv=getinv)
+    val$set(x)
+    val
 }
 
 
@@ -12,4 +26,23 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    mcm<-x$getinv()
+    if (!is.null(mcm)) {
+      message("retrieving matrix from cache")
+      return(mcm)
+    }
+    
+    data<-x$get() #grabs x value if not cached
+    mcm<-solve(data)
+    x$setinv(mcm)
+    mcm
+    
 }
+
+# test
+b<-matrix(1:4,2)
+solve(b)
+a<-makeCacheMatrix(b)
+cacheSolve(a)
+d<-makeCacheMatrix(matrix(1:4,2))
+cacheSolve(d)
